d4879ed54837d3d5b648876b07dc25de5aa8f381
Mohamed Mamdouh <mohameddooha2005@gmail.com>
2026-09-15 16:18:34 +0300

    initial commit

---
+++ app/companions/new/page.tsx
@@ -0,0 +1,9 @@
+const NewCompanion = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">NewCompanion</h1>
+        </div>
+    )
+}
+
+export default NewCompanion;
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Sign In</h1>
+        </div>
+    )
+}
+
+export default Page;
